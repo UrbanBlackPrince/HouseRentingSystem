f00317297cdf5b1d4471234d1584bb5b28fb8d49
Gabriel Nikolov <urbanblackprince@gmail.com>
2024-01-24 22:25:24 +0200

    CheckPoint

---
+++ HouseRentingSystem.Web.ViewModels/Agent/BecomeAgentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Common.EntityValidationConstants.Agent;
+
+namespace HouseRentingSystem.Web.ViewModels.Agent
+{
+    public class BecomeAgentViewModel
+    {
+        [Required]
+        [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
+        [Phone]
+        [Display(Name = "Phone")]
+        public string PhoneNumber { get; set; } = null!;
+    }
+}
+ 
