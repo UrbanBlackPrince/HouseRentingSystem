a59570535dfdc6045b4eddc35ea4bfcad5d894fd
Gabriel Nikolov <urbanblackprince@gmail.com>
2024-02-12 22:43:03 +0200

    CheckPoint

--- HouseRentingSystem/wwwroot/js/site.js
+++ HouseRentingSystem/wwwroot/js/site.js
@@ -1,29 +1,24 @@
 ﻿function statistics() {
-    $('#statictics_btn').on('click', function (e) {
-       
+    $('#statistics_btn').on('click', function (e) {
         e.preventDefault();
         e.stopPropagation();
-       
 
         if ($('#statistics_box').hasClass('d-none')) {
             $.get('https://localhost:7242/api/statistics', function (data) {
-                $('#total_houses').text(data.totalHouses + " Houses")
-                $('#total_rents').text(data.totalRents + " Rents")
+                $('#total_houses').text(data.totalHouses + " Houses");
+                $('#total_rents').text(data.totalRents + " Rents");
 
                 $('#statistics_box').removeClass('d-none');
 
                 $('#statistics_btn').text('Hide Statistics');
-                $('#statistics_btn').removeClass('btn-primary');
-                $('#statistics_btn').addClass('btn-danger');
+                $('#statistics_btn').removeClass('btn-primary').addClass('btn-danger');
             });
-        }
-        else {
+        } else {
+         
             $('#statistics_box').addClass('d-none');
 
             $('#statistics_btn').text('Show Statistics');
-            $('#statistics_btn').addClass('btn-danger');
-            $('#statistics_btn').removeClass('btn-primary');
+            $('#statistics_btn').removeClass('btn-danger').addClass('btn-primary');
         }
-
     });
 }
